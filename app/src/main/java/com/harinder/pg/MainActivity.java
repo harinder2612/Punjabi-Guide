4bb8ee6abab7e721c2f9e1c2d0e692105b2db8e6
Harinder Pal Singh <harinder2612@gmail.com>
2017-03-06 04:14:58 +0530

    n

--- app/src/main/java/com/harinder/pg/MainActivity.java
+++ app/src/main/java/com/harinder/pg/MainActivity.java
@@ -5,41 +5,52 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView t1,t2,t3,t4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t1= (TextView) findViewById(R.id.num);
+        t2= (TextView) findViewById(R.id.fam);
+        t3= (TextView) findViewById(R.id.col);
+        t4= (TextView) findViewById(R.id.phr);
+
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,Numbers.class);
+                intent.putExtra("key","Numbers Activity");
+                startActivity(intent);
+
+            }
+        });
+
+        t2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,FamilyMembers.class);
+                startActivity(intent);
+            }
+        });
+
+        t3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,Colors.class);
+                startActivity(intent);
+            }
+        });
+
+        t4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,Phrases.class);
+                startActivity(intent);
+            }
+        });
     }
 
-public void numbers (View view)
-  {
-      Intent intent= new Intent(this,Numbers.class);
-     intent.putExtra("key","Numbers Activity");
-      startActivity(intent);
-
-  }
-
-    public void family (View view)
-    {
-        Intent intent= new Intent(this,Numbers.class);
-        startActivity(intent);
-
-    }
-
-    public void colors (View view)
-    {
-        Intent intent= new Intent(this,Numbers.class);
-        startActivity(intent);
-
-    }
-
-    public void phrases (View view)
-    {
-        Intent intent= new Intent(this,Numbers.class);
-        startActivity(intent);
-
-    }
 }
